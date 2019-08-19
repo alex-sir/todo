357e179a4aeac0a4566b0d78853f1c17e3e40dde
alex-sir <alexcarbajal710@gmail.com>
2019-08-18 23:56:52 -0700

    Updates

---
+++ precache-manifest.b52f85e9ffd436fac8a1a3c7db0559cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c5823fb8aec92629b2a44da4aa35e9",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "11720e113e2eb30314ae",
+    "url": "/todo/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "6ce050e9ec1c4ec61126",
+    "url": "/todo/static/css/main.068df845.chunk.css"
+  },
+  {
+    "revision": "11720e113e2eb30314ae",
+    "url": "/todo/static/js/2.8e8ce327.chunk.js"
+  },
+  {
+    "revision": "6ce050e9ec1c4ec61126",
+    "url": "/todo/static/js/main.2c82dcb0.chunk.js"
+  },
+  {
+    "revision": "263ea585d3801eaba5dc",
+    "url": "/todo/static/js/runtime~main.447bd38d.js"
+  }
+]);
